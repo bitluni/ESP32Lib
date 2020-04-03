8a24beb6ca4365994d31d8d90eb6ccd5338774fe
Martin-Laclaustra <Martin-Laclaustra@github.com>
2020-04-03 09:55:35 +0200

    fix VGANoFramebuffer example for the new interrupt pointers

--- examples/VGANoFrameBuffer/VGANoFrameBuffer.ino
+++ examples/VGANoFrameBuffer/VGANoFrameBuffer.ino
@@ -21,29 +21,69 @@ long frameNumber = 0;
 //Our own VGA Device
 class MyVGA : public VGA14BitI
 {
-  protected:
+	public:
+	//override this routine that is called during init to link the custom interrupt
+	void propagateResolution(const int xres, const int yres)
+	{
+		setResolution(xres, yres);
+		interruptStaticChild = &MyVGA::custominterrupt;
+	}
+	protected:
 	//override frame buffer allocation
 	virtual Color **allocateFrameBuffer()
 	{
 		return 0;
 	}
+	static void custominterrupt(void *arg);
+	static void custominterruptPixelLine(int y, unsigned long *pixels, unsigned long syncBits, void *arg);
+};
 
-	//override the sync callback to count the frames
-	virtual void vSync()
+//custominterrupt just copied from VGA14Bit except the initial and end lines
+void IRAM_ATTR MyVGA::custominterrupt(void *arg)
+{
+	MyVGA * staticthis = (MyVGA *)arg;
+//Modified for MyVGA until here
+
+	//fix for skipped lines due to skipped interupts during wifi activity
+	DMABufferDescriptor *currentDmaBufferDescriptor = (DMABufferDescriptor *)REG_READ(I2S_OUT_EOF_DES_ADDR_REG(staticthis->i2sIndex));
+	staticthis->dmaBufferDescriptorActive = ((uint32_t)currentDmaBufferDescriptor - (uint32_t)staticthis->dmaBufferDescriptors)/sizeof(DMABufferDescriptor);
+	staticthis->currentLine = staticthis->dmaBufferDescriptorActive; //equivalent in this configuration
+
+	int vInactiveLinesCount = staticthis->mode.vFront + staticthis->mode.vSync + staticthis->mode.vBack;
+
+	//render ahead (the lenght of buffered lines)
+	int renderLine = (staticthis->currentLine + staticthis->lineBufferCount) % staticthis->totalLines;
+
+	if (renderLine >= vInactiveLinesCount)
 	{
+		int renderActiveLine = renderLine - vInactiveLinesCount;
+		unsigned long *pixels = &((unsigned long *)staticthis->vActiveLineBuffer[renderActiveLine % staticthis->lineBufferCount])[(staticthis->mode.hSync + staticthis->mode.hBack) / 2];
+		unsigned long base = (staticthis->hsyncBitI | staticthis->vsyncBitI) * 0x10001;
+
+		int y = renderActiveLine / staticthis->mode.vDiv;
+		if (y >= 0 && y < staticthis->mode.vRes)
+//Modified for MyVGA from here
+		staticthis->custominterruptPixelLine(y, pixels, base, arg);
+	}
+
+	if (renderLine == 0)
+	{
+		staticthis->vSyncPassed = true;
+		//added to count the frames
 		frameNumber++;
 	}
+}
 
-	//draw each line
-	void interruptPixelLine(int y, unsigned long *pixels, unsigned long syncBits)
+//Custom rendering routine to draw each line
+void IRAM_ATTR MyVGA::custominterruptPixelLine(int y, unsigned long *pixels, unsigned long syncBits, void *arg)
+{
+	MyVGA * staticthis = (MyVGA *)arg;
+	for (int x = 0; x < staticthis->mode.hRes / 2; x++)
 	{
-		for (int x = 0; x < mode.hRes / 2; x++)
-		{
-			//writing two pixels improves speed drastically (avoids memory reads)
-			pixels[x] = syncBits | rainbow[(x - y + frameNumber) & 255];
-		}
+		//writing two pixels improves speed drastically (avoids memory reads)
+		pixels[x] = syncBits | rainbow[(x - y + frameNumber) & 255];
 	}
-};
+}
 
 //get an instance
 MyVGA vga;
