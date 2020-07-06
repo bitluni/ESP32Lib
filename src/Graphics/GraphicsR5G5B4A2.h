4330fdcfee3dcfcc83cbb3951c572bfcc652f17e
Martin-Laclaustra <Martin-Laclaustra@github.com>
2020-07-06 16:59:27 +0200

    Graphics redesign: GraphicsR5G5B4A2(VGA14BitI) fragmented in components

--- src/Graphics/GraphicsR5G5B4A2.h
+++ src/Graphics/GraphicsR5G5B4A2.h
@@ -11,27 +11,38 @@
 */
 #pragma once
 #include "Graphics.h"
+#include "BufferLayouts/BLpx1sz16sw0sh0.h"
+#include "ColorToBuffer/CTBIdentity.h"
 
-class GraphicsR5G5B4A2: public Graphics<ColorR5G5B4A2, unsigned short>
+class GraphicsR5G5B4A2: public Graphics<ColorR5G5B4A2, unsigned short>, public BLpx1sz16sw0sh0, public CTBIdentity
 {
 	public:
-	typedef unsigned short InternalColor;
+	//TODO:this must be abstracted to inherited class after moving most generic code into Graphics class
+	typedef typename BLpx1sz16sw0sh0::BufferUnit InternalColor;
 
 	GraphicsR5G5B4A2()
 	{
+		//TODO:decide where to move this.
 		frontColor = 0xffff;
 	}
 
+	//TODO:eventually (when it is equal for all subclasses) move into a non-virtual function in Graphics class wrapped in a virtual one
 	virtual void dotFast(int x, int y, Color color)
 	{
-		backBuffer[y][x] = color;
+		//decide x position[sw] -> shift depending (or not) on x[shval] -> mask[bufferdatamask] -> erase bits
+		backBuffer[y][static_sw(x)] &= static_shval(~static_bufferdatamask(), x, y); // delete bits
+		//mask[colormask] -> convert to buffer[coltobuf] -> shift depending (or not) on x[shval] -> decide x position[sw] -> store data
+		backBuffer[y][static_sw(x)] |= static_shval(coltobuf(color & static_colormask(), x, y), x, y); // write new bits
 	}
 
+	//TODO:eventually (when it is equal for all subclasses) move into a non-virtual function in Graphics class wrapped in a virtual one
 	virtual Color getFast(int x, int y)
 	{
-		return backBuffer[y][x];
+		//decide x position[sw] -> retrieve data -> shift depending (or not) on x[shbuf] -> mask[bufferdatamask] -> convert to color[buftocol]
+		return buftocol(static_shbuf(backBuffer[y][static_sw(x)], x, y) & static_bufferdatamask());
 	}
 
+	//TODO:study differences between subclasses and decide where it is optimal to allocate buffer
 	virtual InternalColor** allocateFrameBuffer()
 	{
 		return Graphics::allocateFrameBuffer(xres, yres, (InternalColor)0);
