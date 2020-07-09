42d56c3c84082ba9a94587ba8882cdbeced0bb41
Martin-Laclaustra <Martin-Laclaustra@github.com>
2020-07-09 10:01:20 +0200

    Graphics redesign: GraphicsTextBuffer fragmented in components

--- src/Graphics/GraphicsTextBuffer.h
+++ src/Graphics/GraphicsTextBuffer.h
@@ -11,32 +11,42 @@
 */
 #pragma once
 #include "Graphics.h"
+#include "BufferLayouts/BLpx1sz8sw0sh0.h"
+#include "ColorToBuffer/CTBIdentity.h"
 
-class GraphicsTextBuffer: public Graphics<ColorW8, unsigned char>
+class GraphicsTextBuffer: public Graphics<ColorW8, unsigned char>, public BLpx1sz8sw0sh0, public CTBIdentity
 {
 	public:
-	typedef unsigned char InternalColor; //Color are ASCII codes in this buffer
-	// FUTURE PLANS: OUTPUTCOLOR COULD BE TEMPLATED
+	//TODO:this must be abstracted to inherited class after moving most generic code into Graphics class
+	typedef typename BLpx1sz8sw0sh0::BufferUnit InternalColor;
 	//These are interpreted as 3-bit color:
 	ColorR1G1B1A1X4::Color frontGlobalColor, backGlobalColor;
 
 	GraphicsTextBuffer()
 	{
+		//TODO:decide where to move this.
 		frontColor = 0x30;
 		frontGlobalColor = 0xf;
 		backGlobalColor = 0x0;
 	}
 
+	//TODO:eventually (when it is equal for all subclasses) move into a non-virtual function in Graphics class wrapped in a virtual one
 	virtual void dotFast(int x, int y, Color color)
 	{
-		backBuffer[y][x] = color;
+		//decide x position[sw] -> shift depending (or not) on x[shval] -> mask[bufferdatamask] -> erase bits
+		backBuffer[static_swy(y)][static_swx(x)] &= ~static_shval(static_colormask(), x, y); // delete bits
+		//mask[colormask] -> convert to buffer[coltobuf] -> shift depending (or not) on x[shval] -> decide x position[sw] -> store data
+		backBuffer[static_swy(y)][static_swx(x)] |= static_shval(coltobuf(color & static_colormask(), x, y), x, y); // write new bits
 	}
 
+	//TODO:eventually (when it is equal for all subclasses) move into a non-virtual function in Graphics class wrapped in a virtual one
 	virtual Color getFast(int x, int y)
 	{
-		return backBuffer[y][x];
+		//decide x position[sw] -> retrieve data -> shift depending (or not) on x[shbuf] -> mask[bufferdatamask] -> convert to color[buftocol]
+		return buftocol(static_shbuf(backBuffer[static_swy(y)][static_swx(x)], x, y) & static_colormask());
 	}
 
+	//TODO:study differences between subclasses and decide where it is optimal to allocate buffer
 	virtual InternalColor** allocateFrameBuffer()
 	{
 		return Graphics::allocateFrameBuffer(xres, yres, (InternalColor)32);
