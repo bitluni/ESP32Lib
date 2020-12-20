0da9adbd78078956958c2c40837e576a00484084
Martin-Laclaustra <Martin-Laclaustra@github.com>
2020-12-20 17:53:53 +0100

    Create a header ESP32Video to allow using it separated from audio and game controller

--- src/ESP32Lib.h
+++ src/ESP32Lib.h
@@ -1,33 +1,5 @@
 #pragma once
 
-#include <VGA/VGAMode.h>
-
-#include <VGA/VGA14BitI.h>
-#include <VGA/VGA14Bit.h>
-#include <VGA/VGA6BitI.h>
-#include <VGA/VGA6Bit.h>
-#include <VGA/VGA3BitI.h>
-#include <VGA/VGA3Bit.h>
-#include <VGA/VGA1BitI.h>
-#include <VGA/VGATextI.h>
-#include <VGA/VGA8BitDAC.h>
-
-#include <Composite/CompMode.h>
-
-//#include <Composite/CompositeL8.h>
-//#include <Composite/CompositePAL8.h>
-#include <Composite/CompositeGrayDAC.h>
-#include <Composite/CompositeGrayLadder.h>
-#include <Composite/CompositeColorDAC.h>
-#include <Composite/CompositeColorLadder.h>
-
-#include <LED/SerialLED.h>
-#include <LED/ParallelLED.h>
-#include <LED/ParallelLEDGraphics.h>
-
-#include <Graphics/Sprites.h>
-#include <Graphics/Mesh.h>
-#include <Graphics/Sprites.h>
-#include <Graphics/Animation.h>
+#include <ESP32Video.h>
 #include <Controller/GameControllers.h>
 #include <Audio/AudioOutput.h>
---
+++ src/ESP32Video.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <VGA/VGAMode.h>
+
+#include <VGA/VGA14BitI.h>
+#include <VGA/VGA14Bit.h>
+#include <VGA/VGA6BitI.h>
+#include <VGA/VGA6Bit.h>
+#include <VGA/VGA3BitI.h>
+#include <VGA/VGA3Bit.h>
+#include <VGA/VGA1BitI.h>
+#include <VGA/VGATextI.h>
+#include <VGA/VGA8BitDAC.h>
+
+#include <Composite/CompMode.h>
+
+#include <Composite/CompositeGrayDAC.h>
+#include <Composite/CompositeGrayLadder.h>
+#include <Composite/CompositeColorDAC.h>
+#include <Composite/CompositeColorLadder.h>
+
+#include <LED/SerialLED.h>
+#include <LED/ParallelLED.h>
+#include <LED/ParallelLEDGraphics.h>
+
+#include <Graphics/Sprites.h>
+#include <Graphics/Mesh.h>
+#include <Graphics/Sprites.h>
+#include <Graphics/Animation.h>
