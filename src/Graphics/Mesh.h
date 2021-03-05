6ae8f631d94d1704391fe922fb7e6b50723f64fa
Martin-Laclaustra <Martin-Laclaustra@github.com>
2021-03-05 21:07:34 +0100

    3D Engine: Modify basicTriangleShaderNormals() in Mesh.h to use a generic color function (previous version was hardcoded for R5G5B4)

--- src/Graphics/Mesh.h
+++ src/Graphics/Mesh.h
@@ -71,8 +71,8 @@ class Mesh
 		const float L[3] = {0, 0, -1};
 
 		float NdotL = nx * L[0] + ny * L[1] + nz * L[2];
-		if(NdotL < 0) NdotL = 0; 
-		return  int(NdotL * (color & 0x1f)) | (int(NdotL * ((color >> 5) & 0x1f)) << 5) | (int(NdotL * ((color >> 10) & 0xf)) << 10);
+		if(NdotL < 0) NdotL = 0;
+		return Graphics::static_RGBA(NdotL * Graphics::static_R(color), NdotL * Graphics::static_G(color), NdotL * Graphics::static_B(color),255);
 	}
 
 	void drawTriangles(Engine3D<Graphics> &e, Color color = -1, triangleShader ts = 0)
