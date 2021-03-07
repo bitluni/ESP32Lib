6eace60bc6f2471dae5f689c744efb5a2f4b10f0
Martin-Laclaustra <Martin-Laclaustra@github.com>
2021-03-07 10:02:15 +0100

    Composite modes update with comments for incompatible drivers

--- src/Composite/CompMode.cpp
+++ src/Composite/CompMode.cpp
@@ -43,6 +43,42 @@ const ModeComposite CompMode::MODEPAL288P(12, 38, 62, 400,  1,  6, 5, 5,  15, 28
 //312 lines/field-progressiveframe, 19.968 ms/field, 50.080 Hz vSync
 
 
+const ModeComposite CompMode::MODEPALHalf144P(6, 19, 31, 200,  1,  6, 5, 5,  15, 288, 0, 0, 0, 0, 2, 4000000);
+//312 line / 50 frame
+//288 active lines
+
+//250 ns/pix
+//256 pix/line, 64 us/line, 15.625 kHz hSync, 4.75 us hSync
+//312 lines/field-progressiveframe, 19.968 ms/field, 50.080 Hz vSync
+
+
+const ModeComposite CompMode::MODEPALQuarter144P(8, 10, 14, 96,  1,  6, 5, 5,  15, 288, 0, 0, 0, 0, 2, 2000000);
+//312 line / 50 frame
+//288 active lines
+
+//500 ns/pix
+//128 pix/line, 64 us/line, 15.625 kHz hSync, 4.75 us hSync
+//312 lines/field-progressiveframe, 19.968 ms/field, 50.080 Hz vSync
+
+
+const ModeComposite CompMode::MODEPAL288Pmax(22, 64, 74, 704,  1,  6, 5, 5,  15, 288, 0, 0, 0, 0, 1, 13333333);
+//312 line / 50 frame
+//288 active lines
+
+//75 ns/pix
+//864 pix/line, 64.800 us/line, 15.432099 kHz hSync, 4.8 us hSync
+//312 lines/field-progressiveframe, 20.2176 ms/field, 49.462 Hz vSync
+
+
+const ModeComposite CompMode::MODEPAL288Pmin(4, 5, 7, 48,  1,  6, 5, 5,  15, 288, 0, 0, 0, 0, 1, 1000000);
+//312 line / 50 frame
+//288 active lines
+
+//1000 ns/pix
+//64 pix/line, 64 us/line, 15.625 kHz hSync, 5 us hSync
+//312 lines/field-progressiveframe, 19.968 ms/field, 50.080 Hz vSync
+
+
 const ModeComposite CompMode::MODEPAL576I(12, 38, 62, 400,  1,  5, 5, 5,  15, 288, 1, 0, 2, 1, 1, 8000000);
 //625 line / 25 frame
 //576 active lines
@@ -54,25 +90,18 @@ const ModeComposite CompMode::MODEPAL576I(12, 38, 62, 400,  1,  5, 5, 5,  15, 28
 //312.5 lines/field, 20 ms/field, 50 Hz vSync
 
 
-const ModeComposite CompMode::MODEPAL288Pmax(20, 64, 88, 704,  1,  6, 5, 5,  15, 288, 0, 0, 0, 0, 1, 13333333);
-//312 line / 50 frame
-//288 active lines
+const ModeComposite CompMode::MODEPAL576Imax(22, 64, 74, 704,  1,  5, 5, 5,  15, 288, 1, 0, 2, 1, 1, 13333333);
+//625 line / 25 frame
+//576 active lines
+
+// every field has 1 half-line less (vPreEqHL 6->5) and 4 half-lines (1+2+0+1) are added to vsync part
 
 //75 ns/pix
-//856 pix/line, 64.200 us/line, 15.576324 kHz hSync, 4.8 us hSync
-//312 lines/field-progressiveframe, 20.0304 ms/field, 49.924 Hz vSync
+//864 pix/line, 64.800 us/line, 15.432099 kHz hSync, 4.8 us hSync
+//312.5 lines/field, 20.25 ms/field, 49.383 Hz vSync
 
 
-const ModeComposite CompMode::MODEPAL288Pmin(2, 5, 5, 52,  1,  6, 5, 5,  15, 288, 0, 0, 0, 0, 1, 1000000);
-//312 line / 50 frame
-//288 active lines
-
-//1000 ns/pix
-//64 pix/line, 64 us/line, 15.625 kHz hSync, 5 us hSync
-//312 lines/field-progressiveframe, 19.968 ms/field, 50.080 Hz vSync
-
-
-const ModeComposite CompMode::MODEPAL576Imin(2, 5, 5, 52,  1,  5, 5, 5,  15, 288, 1, 0, 2, 1, 1, 1000000);
+const ModeComposite CompMode::MODEPAL576Imin(4, 5, 7, 48,  1,  5, 5, 5,  15, 288, 1, 0, 2, 1, 1, 1000000);
 //625 line / 25 frame
 //576 active lines
 
@@ -101,22 +130,22 @@ const ModeComposite CompMode::MODEPAL576Idiv3(12, 38, 62, 400,  1,  5, 5, 5,  15
 //color burst: 5*1000000*63/88 = 3579545.4545 Hz +/- 10 Hz
 //line: 63.5555 us = 1.5 us + 4.7 us + 4.5 us + 52.8555 us
 
-const ModeComposite CompMode::MODENTSC240P(12, 38, 58, 400,  1,  6, 6, 6,  12, 240, 0, 0, 0, 0, 1, 8000000);
+const ModeComposite CompMode::MODENTSC240P(12, 38, 54, 400,  1,  6, 6, 6,  12, 240, 0, 0, 0, 0, 1, 8000000);
 //262 line / 60 frame
 //240 active lines
 
 //125 ns/pix
-//508 pix/line, 63.5 us/line, 15.74803 kHz hSync, 4.75 us hSync
-//262 lines/field-progressiveframe, 16.637 ms/field, 60.107 Hz vSync
+//504 pix/line, 63 us/line, 15.87302 kHz hSync, 4.75 us hSync
+//262 lines/field-progressiveframe, 16.506 ms/field, 60.584 Hz vSync
 
 
-const ModeComposite CompMode::MODENTSC480I(12, 38, 58, 400,  1,  6, 6, 6,  12, 240, 0, 0, 1, 1, 1, 8000000);
-//525 line / 30 frame
-//480 active lines
+const ModeComposite CompMode::MODENTSCHalf120P(6, 19, 23, 200,  1,  6, 6, 6,  12, 240, 0, 0, 0, 0, 2, 4000000);
+//262 line / 60 frame
+//240 active lines
 
-//125 ns/pix
-//508 pix/line, 63.5 us/line, 15.74803 kHz hSync, 4.75 us hSync
-//262.5 lines/field, 16.668750 ms/field, 59.9925 Hz vSync
+//250 ns/pix
+//248 pix/line, 62 us/line, 16.12903 kHz hSync, 4.75 us hSync
+//262 lines/field-progressiveframe, 16.244 ms/field, 61.561 Hz vSync
 
 
 const ModeComposite CompMode::MODENTSC240Pmax(20, 64, 76, 688,  1,  6, 6, 6,  12, 240, 0, 0, 0, 0, 1, 13333333);
@@ -128,12 +157,31 @@ const ModeComposite CompMode::MODENTSC240Pmax(20, 64, 76, 688,  1,  6, 6, 6,  12
 //262 lines/field-progressiveframe, 16.6632 ms/field, 60.0125 Hz vSync
 
 
+const ModeComposite CompMode::MODENTSC480I(12, 38, 54, 400,  1,  6, 6, 6,  12, 240, 0, 0, 1, 1, 1, 8000000);
+//525 line / 30 frame
+//480 active lines
+
+//125 ns/pix
+//504 pix/line, 63 us/line, 15.87302 kHz hSync, 4.75 us hSync
+//262.5 lines/field, 16.5375 ms/field, 60.4686 Hz vSync
+
+
+const ModeComposite CompMode::MODENTSC480Imax(20, 64, 76, 688,  1,  6, 6, 6,  12, 240, 0, 0, 1, 1, 1, 13333333);
+//525 line / 30 frame
+//480 active lines
+
+//75 ns/pix
+//848 pix/line, 63.6 us/line, 15.72327 kHz hSync, 4.8 us hSync
+//262.5 lines/field, 16.695 ms/field, 59.8982 Hz vSync
+
+
 //===========
 //COLOR MODES
 //===========
-const ModeComposite CompMode::MODEPALColor288P(18, 56, 94, 604,  1+78,  6, 5, 5,  15+10, 288-88, 0, 0, 0, 0, 1, 12051950,10,25,4433619,true);
-const ModeComposite CompMode::MODENTSCColor240P(20, 56, 72, 652,  1+36,  6, 6, 6,  12+4, 240-40, 0, 0, 0, 0, 1, 12560000,13,32,3579545,false);
-
+const ModeComposite CompMode::MODEPALColor288P(18, 56, 94, 600,  1+78,  6, 5, 5,  15+10, 288-88, 0, 0, 0, 0, 1, 11989505,10,25,4433619,true);
+const ModeComposite CompMode::MODEPALColor288Pmid(14, 34, 40, 376,  1,  6, 5, 5,  15, 288, 0, 0, 0, 0, 1, 7243659,6,20,4433619,true);
+const ModeComposite CompMode::MODENTSCColor240P(24, 56, 72, 648,  1+36,  6, 6, 6,  12+4, 240-40, 0, 0, 0, 0, 1, 12560000,13,32,3579545,false);
+const ModeComposite CompMode::MODENTSCColor120P(24, 56, 72, 648,  1,  6, 6, 6,  12, 240, 0, 0, 0, 0, 2, 12560000,13,32,3579545,false);
 
 //===========
 //OLD MODES
--- src/Composite/CompMode.h
+++ src/Composite/CompMode.h
@@ -16,22 +16,25 @@ class CompMode
   public:
 	CompMode(){};
 
-	static const ModeComposite MODEPAL288P;
-	static const ModeComposite MODEPAL576I;
-	static const ModeComposite MODEPALColor288P;
+	static const ModeComposite MODEPAL288P; // OOM in CompositeGrayPDM8/4-8266
+	static const ModeComposite MODEPALHalf144P; // insufficient sync in CompositeGrayPDM2-8266
+	static const ModeComposite MODEPALQuarter144P; // CompositeGrayPDM8-8266 (stripes), CompositeGrayPDM4/2-8266 (unusable stripes)
+	static const ModeComposite MODEPAL288Pmax; // OOM in DAC, in 8266
+	static const ModeComposite MODEPAL288Pmin; // does not work in ladder, CompositeGrayPDM8/4-8266 (unusable stripes), insufficient sync in CompositeGrayPDM2-8266
+	static const ModeComposite MODEPAL576I; // OOM in DAC, in 8266
+	static const ModeComposite MODEPAL576Imax; // OOM in DAC, in Ladder, in 8266
+	static const ModeComposite MODEPAL576Imin; // does not work in ladder, OOM in CompositeGrayPDM8-8266, CompositeGrayPDM4-8266 (unusable stripes), insufficient sync in CompositeGrayPDM2-8266
+	static const ModeComposite MODEPAL576Idiv3; // OOM in CompositeGrayPDM8/4-8266
+	static const ModeComposite MODEPALColor288P; // OOM in CompositeGrayPDM8/4-8266
+	static const ModeComposite MODEPALColor288Pmid; // OOM in CompositeGrayPDM8/4-8266
 
-	static const ModeComposite MODENTSC240P;
-	static const ModeComposite MODENTSC480I;
-	static const ModeComposite MODENTSCColor240P;
-
-	static const ModeComposite MODEPAL288Pmax;
-
-	static const ModeComposite MODEPAL288Pmin;
-	static const ModeComposite MODEPAL576Imin;
-
-	static const ModeComposite MODEPAL576Idiv3;
-
+	static const ModeComposite MODENTSC240P; // OOM in CompositeGrayPDM8/4-8266, incorrect sync in CompositeGrayPDM2-8266
+	static const ModeComposite MODENTSCHalf120P; // incorrect sync in CompositeGrayPDM?-8266
 	static const ModeComposite MODENTSC240Pmax;
+	static const ModeComposite MODENTSC480I; // OOM in DAC, in 8266
+	static const ModeComposite MODENTSC480Imax; // OOM in DAC, in Ladder, in 8266
+	static const ModeComposite MODENTSCColor240P;
+	static const ModeComposite MODENTSCColor120P;
 
 	//OLD MODES
 
