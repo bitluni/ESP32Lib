a0552936120b08c3c0ea27beb624fa1063e109f9
Martin-Laclaustra <Martin-Laclaustra@github.com>
2021-03-13 20:03:52 +0100

    Update ESP32Video.h with recently added drivers

--- src/ESP32Video.h
+++ src/ESP32Video.h
@@ -1,25 +1,36 @@
 #pragma once
 
+//== VGA ==
 #include <VGA/VGAMode.h>
 
-#include <VGA/VGA14BitI.h>
+//DMA-only drivers
 #include <VGA/VGA14Bit.h>
-#include <VGA/VGA6BitI.h>
 #include <VGA/VGA6Bit.h>
-#include <VGA/VGA3BitI.h>
 #include <VGA/VGA3Bit.h>
+#include <VGA/VGA8BitDAC.h>
+//Interrupt-based drivers
+#include <VGA/VGA14BitI.h>
+#include <VGA/VGA6BitI.h>
+#include <VGA/VGA3BitI.h>
 #include <VGA/VGA1BitI.h>
 #include <VGA/VGATextI.h>
-#include <VGA/VGA8BitDAC.h>
 
+//== COMPOSITE ==
 #include <Composite/CompMode.h>
 
+//DMA-only drivers
 #include <Composite/CompositeGrayDAC.h>
 #include <Composite/CompositeGrayLadder.h>
 #include <Composite/CompositeColorDAC.h>
 #include <Composite/CompositeColorLadder.h>
 #include <Composite/CompositeColorDACMemory.h>
 #include <Composite/CompositeColorLadderMemory.h>
+#include <Composite/CompositeGrayPDM8.h>
+#include <Composite/CompositeGrayPDM4.h>
+#include <Composite/CompositeGrayPDM2.h>
+//Interrupt-based drivers
+#include <Composite/CompositeGrayDACI.h>
+#include <Composite/CompositeGrayLadderI.h>
 
 #include <LED/SerialLED.h>
 #include <LED/ParallelLED.h>
