d0f3527da359ca47438799d23d5cb50a436c439b
Martin-Laclaustra <Martin-Laclaustra@github.com>
2021-03-30 00:19:51 +0200

    Graphics: Fix GraphicsX6S2W8RangedSwapped for VGA8BitDAC where clear did not work for non-negative sync VGA modes

--- src/Graphics/GraphicsX6S2W8RangedSwapped.h
+++ src/Graphics/GraphicsX6S2W8RangedSwapped.h
@@ -28,7 +28,8 @@ class GraphicsX6S2W8RangedSwapped: public Graphics<ColorW8, BLpx1sz16sw1sh8, CTB
 
 	virtual void clear(Color color = 0)
 	{
-		BufferGraphicsUnit newColor = (BufferGraphicsUnit)static_shval(coltobuf(color & static_colormask(), 0, 0), 0, 0) | SBits;
+		BufferGraphicsUnit bufferUnaffectedBits = (backBuffer[0][0])&( ~graphics_shval(Graphics::static_bufferdatamask(), 0, 0) );
+		BufferGraphicsUnit newColor = (BufferGraphicsUnit)static_shval(coltobuf(color & static_colormask(), 0, 0), 0, 0) | bufferUnaffectedBits;
 		for (int y = 0; y < this->yres; y++)
 			for (int x = 0; x < this->xres; x++)
 				backBuffer[y][x] = newColor;
